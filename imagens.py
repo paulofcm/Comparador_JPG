19db28d347796e32cf3eb43da972674e2ccb2959
manoelpedroza <manoelpedroza.pe@gmail.com>
2021-10-26 09:48:52 -0300

    Novos ajustes

--- imagens.py
+++ imagens.py
@@ -39,65 +39,69 @@ def deletar_imagens():
             i.close()
             os.remove(i.name)
 
-def comparar_imagens():
+def comparar_imagens(): #5
     messagebox.showinfo("Etapa 1", "Escolha a imagem para ser comparada")
     imagem_A = filedialog.askopenfile()
     i = True
-    try:
-        imagem_X = PIL.Image.open(imagem_A.name)
-        h1 = imagehash.phash(imagem_X)
-
-    except PIL.UnidentifiedImageError:
-        i = False
-        print("A primeira imagem está corrompida")
-    if i:
-        messagebox.showinfo("Etapa 2", "Escolha a segunda imagem para comparação")
-        imagem_B = filedialog.askopenfile()
+    if imagem_A != None:
         try:
-            imagem_Y = PIL.Image.open(imagem_B.name)
-            h2 = imagehash.phash(imagem_Y)
+            imagem_X = PIL.Image.open(imagem_A.name)
+            h1 = imagehash.phash(imagem_X)
 
-            resultado = 100 - ((h1 - h2) / len(h2.hash) ** 2 * 100)
-            print("A imagem  em", imagem_A.name, "\ntem ", resultado, "% de similaridade com a imagem ", imagem_B.name)
         except PIL.UnidentifiedImageError:
-            print("A segunda imagem está corrompida")
+            i = False
+            print("A primeira imagem está corrompida")
+        if i:
+            messagebox.showinfo("Etapa 2", "Escolha a segunda imagem para comparação")
+            imagem_B = filedialog.askopenfile()
+            if imagem_B != None:
+                try:
+                    imagem_Y = PIL.Image.open(imagem_B.name)
+                    h2 = imagehash.phash(imagem_Y)
+
+                    resultado = 100 - ((h1 - h2) / len(h2.hash) ** 2 * 100)
+                    print("A imagem  em", imagem_A.name, "\ntem ", resultado, "% de similaridade com a imagem ", imagem_B.name)
+                except PIL.UnidentifiedImageError:
+                    print("A segunda imagem está corrompida")
 
 
 
-def achar_similaridade():
+def achar_similaridade(): #6
     n=3
     list_dic = {}
     dic_hash = {}
     dic_mapeado = {}
     messagebox.showinfo("Etapa 1", "Escolha a imagem para ser comparada")
     imagem_A = filedialog.askopenfile()
-    str = imagem_A.name
-    sp = str.split("/")
-    nome_da_imagem = sp[-1]
-    i = verifica_jpg(imagem_A.name)
-    if i:
-        try:
-            imagem_X = PIL.Image.open(imagem_A.name)
-            h1 = imagehash.phash(imagem_X)
-        except PIL.UnidentifiedImageError:
-            i = False
-            print("A imagem ", imagem_X, " esta corrompida")
+    if imagem_A != None:
+        str = imagem_A.name
+        sp = str.split("/")
+        nome_da_imagem = sp[-1]
+        i = verifica_jpg(imagem_A.name)
         if i:
-            messagebox.showinfo("Etapa 2", "Escolha o banco de imagens  para comparação")
-            imagens = filedialog.askdirectory()
-            for file in os.listdir(imagens):
-                try:
-                    f = PIL.Image.open(imagens+ "/" + file)
-                    h = imagehash.phash(f)
-                    dic_hash[file]=h
-                except PIL.UnidentifiedImageError:
-                    print("Esta imagem ", file, " está corrompida e será ignorada")
+            try:
+                imagem_X = PIL.Image.open(imagem_A.name)
+                h1 = imagehash.phash(imagem_X)
+            except PIL.UnidentifiedImageError:
+                i = False
+                print("A imagem ", imagem_X, " esta corrompida")
+            if i:
+                messagebox.showinfo("Etapa 2", "Escolha o banco de imagens  para comparação")
+                imagens = filedialog.askdirectory()
+                if imagens != "":
+                    for file in os.listdir(imagens):
+                        try:
+                            f = PIL.Image.open(imagens+ "/" + file)
+                            h = imagehash.phash(f)
+                            dic_hash[file]=h
+                        except PIL.UnidentifiedImageError:
+                            print("Esta imagem ", file, " está corrompida e será ignorada")
 
-        for i in dic_hash:
-            resultado = 100- ((h1 - dic_hash[i]) / len(dic_hash[i].hash) ** 2*100)
-            list_dic[i] = resultado
-        dic_mapeado = dict(sorted(list_dic.items(), key=itemgetter(1), reverse=True)[:n])
-        print("\nOs ",n," maiores valores são: " , dic_mapeado)
+                    for i in dic_hash:
+                        resultado = 100- ((h1 - dic_hash[i]) / len(dic_hash[i].hash) ** 2*100)
+                        list_dic[i] = resultado
+                    dic_mapeado = dict(sorted(list_dic.items(), key=itemgetter(1), reverse=True)[:n])
+                    print("\nOs ",n," maiores valores são: " , dic_mapeado)
 
 
 def verifica_jpg(str):
